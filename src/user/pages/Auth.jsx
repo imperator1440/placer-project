b30844fb5b9d8205292902acaf7fa5a626733f29
imperator1440 <imperator1440@gmail.com>
2024-01-24 17:49:48 +0300

    add error handling

--- src/user/pages/Auth.jsx
+++ src/user/pages/Auth.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, Fragment } from 'react';
 import { useForm } from '../../shared/hooks/form-hook';
 
 import Card from '../../shared/components/UIElements/Card';
@@ -70,6 +70,10 @@ const Auth = () => {
         });
 
         const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
         setIsLoading(false);
         auth.login();
       } catch (err) {
@@ -79,48 +83,55 @@ const Auth = () => {
     }
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card className='authentication'>
-      {isLoading && <LoadingSpinner asOverlay/>}
-      <h2>Login Required</h2>
-      <hr />
-      <form onSubmit={authSubmitHandler}>
-        {!isLoginMode && (
+    <Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      <Card className='authentication'>
+        {isLoading && <LoadingSpinner asOverlay/>}
+        <h2>Login Required</h2>
+        <hr />
+        <form onSubmit={authSubmitHandler}>
+          {!isLoginMode && (
+            <Input
+              element='input'
+              id='name' type='text'
+              label='Name'
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText='Please enter a name.'
+              onInput={inputHandler}
+            />
+          )}
           <Input
+		  	  	id='email'
             element='input'
-            id='name' type='text'
-            label='Name'
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText='Please enter a name.'
-            onInput={inputHandler}
-          />
-        )}
-        <Input
-			  	id='email'
-          element='input'
-			  	type='email'
-			  	label='E-mail'
-			  	validators={[VALIDATOR_EMAIL()]}
-			  	errorText='Please enter a valid email.'
-			  	onInput={inputHandler}
-			  />
-        <Input
-			  	id='password'
-          element='input'
-			  	type='password'
-			  	label='Password'
-			  	validators={[VALIDATOR_MINLENGTH(5)]}
-			  	errorText='Please enter a valid password, at least 5 characters.'
-			  	onInput={inputHandler}
-			  />
-        <Button type='submit' disabled={!formState.isValid}>
-          {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+		  	  	type='email'
+		  	  	label='E-mail'
+		  	  	validators={[VALIDATOR_EMAIL()]}
+		  	  	errorText='Please enter a valid email.'
+		  	  	onInput={inputHandler}
+		  	  />
+          <Input
+		  	  	id='password'
+            element='input'
+		  	  	type='password'
+		  	  	label='Password'
+		  	  	validators={[VALIDATOR_MINLENGTH(5)]}
+		  	  	errorText='Please enter a valid password, at least 5 characters.'
+		  	  	onInput={inputHandler}
+		  	  />
+          <Button type='submit' disabled={!formState.isValid}>
+            {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
         </Button>
-      </form>
-      <Button inverse onClick={switchModeHandler}>
-        SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
-      </Button>
-    </Card>
+      </Card>
+    </Fragment>
   );
 };
 

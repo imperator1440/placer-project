15a99ac597564be0a7f6e7b61271948b8a249661
imperator1440 <imperator1440@gmail.com>
2024-01-26 20:56:31 +0300

    add adding places in db

--- src/user/pages/Auth.jsx
+++ src/user/pages/Auth.jsx
@@ -55,7 +55,7 @@ const Auth = () => {
 
     if (isLoginMode) {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           'http://localhost:5000/api/users/login',
           'POST',
           JSON.stringify({
@@ -66,11 +66,11 @@ const Auth = () => {
             'Content-Type': 'application/json'
           }
         );
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {}
     } else {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           'http://localhost:5000/api/users/signup',
           'POST',
           JSON.stringify({
@@ -82,8 +82,7 @@ const Auth = () => {
             'Content-Type': 'application/json',
           },
         );
-
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {}
     }
   };

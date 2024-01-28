0b6642fe0dbde108a26fd6df28c465a2d1edaa3a
imperator1440 <imperator1440@gmail.com>
2024-01-28 14:49:52 +0300

    add loading places by users from back end

--- src/places/pages/UserPlaces.jsx
+++ src/places/pages/UserPlaces.jsx
@@ -1,38 +1,37 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState, Fragment } from 'react';
+import { useParams } from 'react-router-dom';
+import { useHttpClient } from '../../shared/hooks/http.hook';
 
-import PlaceList from "../components/PlaceList/PlaceList";
-
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky skrapers in the world!',
-    imageUrl: 'https://media.timeout.com/images/101705309/750/422/image.jpg',
-    address: '20 W 34th St., New York, NY 10001',
-    location: {
-      lat: 40.7484405,
-      lng: -73.9856644
-    },
-    creator: 'u1'
-  },
-  {
-    id: 'p2',
-    title: 'Tour Eiffel',
-    description: 'One of the most famous buildings in Paris!',
-    imageUrl: 'https://cdn.sortiraparis.com/images/80/83517/756379-visuel-paris-tour-eiffel.jpg',
-    address: 'Champ de Mars, 5 Av. Anatole France, 75007 Paris',
-    location: {
-      lat: 48.8583701,
-      lng: 2.2944813
-    },
-    creator: 'u2'
-  }
-];
+import PlaceList from '../components/PlaceList/PlaceList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const UserPlaces = () => { 
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
-  return <PlaceList items={loadedPlaces} />;
+
+  useEffect(() => {
+    const fetchPlaces = async () => { 
+      try {
+        const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`,);
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    };
+  }, [sendRequest, userId]);
+
+  return (
+    <Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </Fragment>
+  );
 };
 
 export default UserPlaces;
